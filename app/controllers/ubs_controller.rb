0507b802f83fe56e4d316d39366a62b271aee292
bahartmann <barbarahartma@gmail.com>
2018-04-26 20:10:03 -0300

    Add request to show an UBS

--- app/controllers/ubs_controller.rb
+++ app/controllers/ubs_controller.rb
@@ -1,6 +1,18 @@
 class UbsController < ApplicationController
+  before_action :set_ubs, only: [:show]
+
   def index
     @ubs = Ubs.all
     json_response(@ubs)
+  end
+
+  def show
+    json_response(@ubs)
+  end
+
+  private
+
+  def set_ubs
+    @ubs = Ubs.find(params[:id])
   end
 end
--- spec/requests/ubs_spec.rb
+++ spec/requests/ubs_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe 'UBS API', type: :request do
 
@@ -17,4 +18,32 @@ RSpec.describe 'UBS API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /ubs/:id' do
+    before { get "/ubs/#{ubs_id}" }
+
+    context 'when the ubs exists' do
+      it 'returns ubs' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(ubs_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the ubs does not exist' do
+      let(:ubs_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(json['message']).to match(/Couldn't find Ubs with 'id'=100/)
+      end
+    end
+  end
+
 end
